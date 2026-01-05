c6a8d7af6b74241e7103034b038f8fde3f7cdcc6
jhonclinton <134811672+jhonclinton@users.noreply.github.com>
2026-01-04 22:55:41 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'sillu-ia-v1';
+// Lista de archivos a "congelar" para que funcionen offline
+const ASSETS = [
+  './',
+  './index.html',
+  './modelo_puno_final.json',
+  './libs/tf.min.js',
+  './libs/mobilenet.js',
+  './libs/knn-classifier.js'
+];
+
+// Instalación: Guarda los archivos en la memoria del navegador
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Interceptor: Cuando MobileNet pida algo a internet, el SW lo entrega desde el disco
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
